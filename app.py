07d5a21f0a2c99afe8e9fdc5f83c7c5ed0250b46
Joanestb <jbarnes@dbasystems.com>
2022-05-07 11:39:22 -0500

    After Week 3

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template("index.html")
+	myH1 = "Week2"
+	return render_template("index.html", TheH1 = myH1)
 
 @app.route('/about')
 def about():
-	return render_template("about.html")
+	names = ["Joe", "Bill", "Wes"]
+	return render_template("about.html", names=names)
