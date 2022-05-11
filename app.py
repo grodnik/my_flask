c1b83e37ac3f07de1a04089eae6d8be22bb8b404
Joanestb <jbarnes@dbasystems.com>
2022-05-11 15:58:04 -0500

    Up thru 1150

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-
 def index():
 	myH1 = "Projects"
 	return render_template("index.html", TheH1 = myH1)
@@ -14,4 +13,12 @@ def projects():
 	names = ["Refurbish", "Fire Suppression", "Water Valves"]
 	return render_template("projects.html", names=names)
 
+@app.route('/addproject')
+def addproject():
+	title = "Add Project"
+	return render_template("addproject.html", title=title)
 
+@app.route('/dispproject', methods=["POST"])
+def dispproject():
+	title = "Display Project"
+	return render_template("dispproject.html", title=title)
