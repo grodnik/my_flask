bb28dbc06d193504c34b69f3bd7ffbc72e060271
Joanestb <jbarnes@dbasystems.com>
2022-05-11 14:40:07 -0500

    basic projects list works

--- app.py
+++ app.py
@@ -3,14 +3,15 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
+
 def index():
-	myH1 = "Week2"
+	myH1 = "Projects"
 	return render_template("index.html", TheH1 = myH1)
 
-@app.route('/about')
-def about():
-	names = ["Joe", "Bill", "Wes"]
-	return render_template("about.html", names=names)
-
+@app.route('/projects')
+def projects():
+	title = "List Projects"
+	names = ["Refurbish", "Fire Suppression", "Water Valves"]
+	return render_template("projects.html", names=names)
 
 
